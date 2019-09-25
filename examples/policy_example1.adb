c65c16013fc17c2db26a26b713adb1c264a8eb1d
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2019-09-25 18:03:52 +0200

    examples: Rework source filtering example
    Use a local Protected_Policy_DB instance for source filtering and
    extend code to illustrate how the default loglevel works.

--- examples/policy_example1.adb
+++ examples/policy_example1.adb
@@ -5,27 +5,28 @@ use Alog;
 
 --  Alog source loglevel policy example.
 procedure Policy_Example1 is
-   Log : Logger.Instance (Init => True);
+   Src_Filter : Policy_DB.Protected_Policy_DB;
+   Log        : Logger.Instance (Init => True);
 begin
    --  Set default loglevel to 'Info'.
-   Policy_DB.Set_Default_Loglevel (Level => Info);
+   Src_Filter.Set_Default_Loglevel (Level => Info);
    --  Set source specific loglevel for all 'Example' sources to 'Debug'.
-   Policy_DB.Set_Loglevel (Identifier => "Example.*",
-                           Level      => Debug);
+   Src_Filter.Set_Loglevel (Identifier => "Example.*",
+                            Level      => Debug);
 
    --  This message will be logged because it matches a source specific
    --  loglevel (Example.*).
-   if Policy_DB.Accept_Src (Identifier => "Example.Source1",
+   if Src_Filter.Accept_ID (Identifier => "Example.Source1",
                             Level      => Debug)
    then
       Log.Log_Message (Source => "Example.Source1",
                        Level  => Debug,
-                       Msg    => "This is a testmessage");
+                       Msg    => "This is a test message");
    end if;
 
    --  This message will not be logged because of the configured default 'Info'
    --  loglevel. There's no configured source loglevel for 'Source2'.
-   if Policy_DB.Accept_Src (Identifier => "Source2",
+   if Src_Filter.Accept_ID (Identifier => "Source2",
                             Level      => Debug)
    then
       Log.Log_Message (Source => "Source2",
@@ -33,10 +34,13 @@ begin
                        Msg    => "This will not be logged");
    end if;
 
-   --  No source specified, will not be logged because of the default 'Info'
-   --  loglevel.
-   if Policy_DB.Accept_Src (Level => Debug) then
-      Log.Log_Message (Level => Debug,
-                       Msg   => "This will not be logged");
+   --  This message will be logged because of the configured default 'Info'
+   --  loglevel. There's no configured source loglevel for 'Source2'.
+   if Src_Filter.Accept_ID (Identifier => "Source2",
+                            Level      => Info)
+   then
+      Log.Log_Message (Source => "Source2",
+                       Level  => Info,
+                       Msg    => "This is another test message");
    end if;
 end Policy_Example1;
