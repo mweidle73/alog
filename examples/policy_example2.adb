24d3cfb9209e3b5b40407d31d5cb545d316c38fa
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2019-09-25 18:38:15 +0200

    Rework destination filtering example
    Adapt to API changes and extend code to illustrate how the default
    loglevel can be used.

--- examples/policy_example2.adb
+++ examples/policy_example2.adb
@@ -1,4 +1,4 @@
-with Alog.Policy_DB;
+with Alog.Dst_Filter;
 with Alog.Logger;
 with Alog.Facilities.File_Descriptor;
 
@@ -6,18 +6,20 @@ use Alog;
 
 --  Alog destination loglevel policy example.
 procedure Policy_Example2 is
-   Log    : Logger.Instance (Init => True);
-   Errors : constant Facilities.File_Descriptor.Handle :=
-     new Facilities.File_Descriptor.Instance;
+   Log : Logger.Instance (Init => True);
+
+   Facility_Name : constant String := "Application_Errors";
+   Errors        : constant Facilities.File_Descriptor.Handle
+     := new Facilities.File_Descriptor.Instance;
 begin
    --  Write all error messages to '/tmp/errors.log'.
    Errors.Set_Logfile (Path => "/tmp/errors.log");
-   Errors.Set_Name (Name => "Application_Errors");
+   Errors.Set_Name (Name => Facility_Name);
    Errors.Toggle_Write_Loglevel (State => True);
 
    --  Set loglevel policy to 'Error' for destination 'Application_Errors'.
-   Policy_DB.Set_Loglevel (Identifier => "Application_Errors",
-                           Level      => Error);
+   Dst_Filter.Set_Loglevel (Name  => Facility_Name,
+                            Level => Error);
 
    Log.Attach_Facility (Facility => Facilities.Handle (Errors));
 
@@ -27,4 +29,14 @@ begin
    --  This message will also be written to the error logfile.
    Log.Log_Message (Level  => Error,
                     Msg    => "This is an error");
+
+   --  Set global loglevel to only log messages with level higher than Warning
+   --  if no facility-specific level is set.
+   Dst_Filter.Set_Default_Level (Level => Warning);
+   --  This message will be filtered out.
+   Log.Log_Message (Level  => Info,
+                    Msg    => "This message will be filtered");
+   --  This message will appear on stdout, but not in the error logfile.
+   Log.Log_Message (Level  => Warning,
+                    Msg    => "This is a warning message");
 end Policy_Example2;
