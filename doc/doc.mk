35a4ccaaf46532b95640fb033465aa0900ca5234
Nicolas Boulenguez <nicolas@debian.org>
2022-11-27 21:33:41 +0100

    doc: Add DFSG compliant architecture diagram
    Replace patch in Debian build system with a doc Makefile option. The
    work mayn be of interest for other redistributors, and is easier to
    maintain upstream.

--- doc/doc.mk
+++ doc/doc.mk
@@ -1,10 +1,16 @@
 # Makefile snippet intended for inclusion by ../Makefile.
 
+# There are two variants of the architecture diagram.
+# * alog-arch.svg is generated from alog-arch.graphml, assuming a
+#   non-free graphml editor and/or converter.
+# * alog-arch-dfsg.svg can be modified with free tools like inkscape.
+DIAGRAM := alog-arch.svg
+
 build-doc:
 	rm -fr doc/html
 	mkdir doc/html
 
-	scour -i doc/arch/alog-arch.svg \
+	scour -i doc/arch/$(DIAGRAM) \
 	  -o doc/html/alog-arch.svg \
 	  --enable-comment-stripping \
 	  --enable-id-stripping \
